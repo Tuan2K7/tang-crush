c64d35a4c28d17cf0488a4e42f3a4e39b7e74e30
Tuan2K7 <92442212+Tuan2K7@users.noreply.github.com>
2021-10-13 19:52:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,12 @@
 const CONFIG = {
     introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Anh có điều này muốn nói với em là
+    Anh đã yêu em từ cái nhìn đầu tiên rồi'
     btnIntro: 'hihi',
     title: 'Em có tin vào tình yêu sét đánh không? Nếu không thì bây giờ đã đến lúc em tin rồi, vì anh chính là nạn nhân của tình yêu đó 😙',
     desc: 'Em có yêu anh không ',
     btnYes: 'Yêu anh rất nhiều <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnNo: 'Không, Anh ạ :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Vì em rất yêu anh <33333333',
