607f0969b34854fb50f3321aa6e7ddce9c4320f9
Tuan2K7 <92442212+Tuan2K7@users.noreply.github.com>
2021-10-13 20:18:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
     btnNo: 'Không, Anh ạ :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Vì em rất yêu anh <33333333',
+    reply: 'Vì anh là người quan tâm em nhất <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Em làm người yêu anh nha.',
     btnAccept: 'Em đồng ý <3',
