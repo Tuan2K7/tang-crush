02ff62fb7873e8ed27ca1f224ab5b88a31240594
Tuan2K7 <92442212+Tuan2K7@users.noreply.github.com>
2021-10-13 19:40:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,15 +5,15 @@ const CONFIG = {
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    title: 'Em có tin vào tình yêu sét đánh không? Nếu không thì bây giờ đã đến lúc em tin rồi, vì anh chính là nạn nhân của tình yêu đó 😙',
+    desc: 'Em có yêu anh không ',
+    btnYes: 'Yêu anh rất nhiều <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Vì em rất yêu anh <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Em làm người yêu anh nha.',
-    btnAccept: 'Okiiiii lun <3',
+    btnAccept: 'Em đồng ý <3',
     messLink: 'https://www.facebook.com/messages/t/100052439573589' 
 }
